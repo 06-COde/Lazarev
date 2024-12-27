fe54027c8c499be3d675dd0d8a1ea11ed330b20f
Shashi Raj <rishav.sinha477@gmail.com>
2024-12-28 01:37:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,89 @@ nav.addEventListener("mouseleave",function(){
 }
 
 navanimation();
+
+function page2Animation(){
+
+    let rightElem = document.querySelectorAll(".right-elem");
+
+rightElem.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity: 1,
+            scale: 1
+        })
+    })
+        elem.addEventListener("mouseleave",function(){
+            gsap.to(elem.childNodes[3],{
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove",function(dets){
+           gsap.to(elem.childNodes[3],{
+            x: dets.x - elem.getBoundingClientRect().x - 50,
+            y: dets.y - elem.getBoundingClientRect().y - 130
+           })
+        })
+})
+}
+
+page2Animation();
+
+function page3VideoAnimation(){
+    let page3Center =  document.querySelector(".page3-center");
+let video  = document.querySelector(".page3 video");
+
+page3Center.addEventListener("click",function(){
+  video.play()
+    gsap.to(video,{
+     transform: "scaleX(1) ScaleY(1)",
+     opacity: 1,
+     borderRadius : 0
+    })
+
+})
+ video.addEventListener("click",function(){
+    video.pause()
+    gsap.to(video,{
+        transform: "scaleX(0.7) scaleY(0)",
+        opacity: 0,
+        borderRadius: 0
+    })
+ })
+}
+
+page3VideoAnimation();
+
+
+function page5ani(){
+    const section = document.querySelectorAll(".sec-right");
+
+section.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
+
+}
+page5ani();
+
+
+
+gsap.from("#btm8-d1 h4",{
+    x:0,
+    duration: 1,
+    scrollTrigger:{
+       trigger : "#btm8-d1" ,
+       scroller : "Body",
+    //    markers: true,
+       start : "top 80%",
+       end : "top 10%",
+       scrub: true,
+    }
+})
